dd798e0c7c530129b15ce6cc68aa39a92faeb4f7
Andrew Kachnic <ajkachnic@protonmail.com>
2021-09-20 06:49:47 -0400

    Remove temporary change

--- main.go
+++ main.go
@@ -10,8 +10,7 @@ import (
 func main() {
 	var wg sync.WaitGroup
 	// Crawl wikipedia
-	seed := spider.HttpSeed{Url: "https://drewdevault.com/"}
-	// seed := spider.HttpSeed{Url: "https://wikipedia.org"}
+	seed := spider.HttpSeed{Url: "https://wikipedia.org"}
 	result, err := seed.Crawl(0, &wg)
 	if err != nil {
 		log.Fatal(err)
